d4bcf12097b2704a9f366ba6a3e4ee452a82f1cd
Emirhan <emirhantokatt@gmail.com>
2023-11-22 14:46:33 +0300

    guncelleme
    .

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:weather_app/views/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,11 +16,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const Scaffold(
-        body: Center(
-          child: Text("Hello World"),
-        ),
-      ),
+      home: const HomeScreen(),
     );
   }
 }
